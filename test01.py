e4860df851e073a0a335342cf6d55ff915f73c11
dbstmd4857 <54461209+dbstmd4857@users.noreply.github.com>
2019-09-28 16:34:03 +0900

    Add files via upload

---
+++ test01.py
@@ -0,0 +1,27 @@
+a = [1 ,2 ,3 ,4]
+b = [5 ,6 ,7 ,8]
+c = [9 ,10, 11, 12]
+
+d = a + b + c
+
+
+for index in range(len(a)):
+    print (a[index])
+
+print()
+
+for index in range(len(b)):
+    print (b[index])
+
+print()
+ 
+for index in range(len(c)):
+    print (c[index])
+    
+print()
+ 
+for index in range(len(d)):
+    print (d[index])
+
+
+
